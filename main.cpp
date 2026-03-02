ead4ca70427f22aa8fd3318f58e25b0623a37b56
AstruntCM <mayer.marton.balint@gmail.com>
2026-03-02 12:37:41 +0100

    Identified errors in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // A const hibásan lett megadva NELEMENTS helyett N_ELEMENTS kellene használni
+    std::cout << '1-100 ertekek duplazasa' // A sor végéről hiányzik a pontosvessző, emelett hibásan lett megadva a string idézőjelei
+    for (int i = 0;) // A for ciklus szintaxisa hibás, hiányzik maga a feltétel és a léptetés
     {
-        b[i] = i * 2;
+        b[i] = i * 2; // 0-val való indulás miatt nem helyes a duplázás, helyette (i + 1) * 2 kellene
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // Hibás a feltétel megadása, hiányzik a N_ELEMENTS
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // Hiányzik a pontosvessző a sor végéről
     }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    std::cout << "Atlag szamitasa: " << std::endl; // Hiányzik a megjelenítésnél hogy hogyan lesz kiszámolva az átlag
+    int atlag; // Az atlag valtozonak nem helyes a tipusa, int helyett float vagy double kellene, nem megfelelő inicializálás
+    for (int i = 0; i < N_ELEMENTS, i++) // feltétel megadása után sima vessző helyett pontosvessző kell
     {
-        atlag += b[i]
+        atlag += b[i] // Sor végéről hiányzik a pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    // Hiányzik a dinamikusan foglalt tömb felszabadítása
     return 0;
 }
