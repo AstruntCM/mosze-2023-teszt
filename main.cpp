cb47bad9be4dcc2e9fe7b6026a9a7f891356fbda
AstruntCM <mayer.marton.balint@gmail.com>
2026-03-02 13:01:25 +0100

    Fixed the errors in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,24 +4,36 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // A const hibásan lett megadva NELEMENTS helyett N_ELEMENTS kellene használni
-    std::cout << '1-100 ertekek duplazasa' // A sor végéről hiányzik a pontosvessző, emelett hibásan lett megadva a string idézőjelei
-    for (int i = 0;) // A for ciklus szintaxisa hibás, hiányzik maga a feltétel és a léptetés
+    int *b = new int[N_ELEMENTS]; 
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        b[i] = i * 2; // 0-val való indulás miatt nem helyes a duplázás, helyette (i + 1) * 2 kellene
+        b[i] = (i + 1) * 2;
     }
-    for (int i = 0; i; i++) // Hibás a feltétel megadása, hiányzik a N_ELEMENTS
+
+    std::cout << "Ertek: "; 
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        std::cout << "Ertek:" // Hiányzik a pontosvessző a sor végéről
+        std::cout << b[i];
+
+        if (i < N_ELEMENTS - 1) {std::cout << ", ";}
+        
+        else {std::cout <<" "<< std::endl;}       
     }    
-    std::cout << "Atlag szamitasa: " << std::endl; // Hiányzik a megjelenítésnél hogy hogyan lesz kiszámolva az átlag
-    int atlag; // Az atlag valtozonak nem helyes a tipusa, int helyett float vagy double kellene, nem megfelelő inicializálás
-    for (int i = 0; i < N_ELEMENTS, i++) // feltétel megadása után sima vessző helyett pontosvessző kell
+
+    double atlag, szum = 0.0; 
+
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        atlag += b[i] // Sor végéről hiányzik a pontosvessző
+        szum += b[i];
     }
-    atlag /= N_ELEMENTS;
+
+    atlag = szum / N_ELEMENTS;
+
     std::cout << "Atlag: " << atlag << std::endl;
-    // Hiányzik a dinamikusan foglalt tömb felszabadítása
+
+    delete[] b;
+
     return 0;
 }
